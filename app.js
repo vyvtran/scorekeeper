54e851f92e27200cec416177401c1a712de181ce
vyvtran <62912798+vyvtran@users.noreply.github.com>
2021-01-23 00:29:27 -0800

    Add files via upload
    Webpage that keeps track of a ping pong game score between two players. Players have the option of choosing what score they will play to. Utilizes JavaScript and HTML as well as the Bulma framework

---
+++ app.js
@@ -0,0 +1,62 @@
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+
+let winningScore = 5;
+let isGameOver = false;
+
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score++;
+
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+
+        player.display.textContent = player.score;
+    }
+}
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2);
+})
+
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1);
+})
+
+//Resets the highest score reached when clicked on 
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    p1.score = 0;
+    p2.score = 0;
+
+    p1.display.textContent = p1Score;
+    p2.display.textContent = p2Score;
+    p2.display.classList.remove('has-text-success', 'has-text-danger');
+    p1.display.classList.remove('has-text-danger', 'has-text-success');
+    p1.button.disabled = false;
+    p2.button.disabled = false;
+}
